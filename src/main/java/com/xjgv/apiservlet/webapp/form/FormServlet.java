c58ff7b489567bc7a25e5318f5c3256fd1442b67
Javier Gomez <javier.gov@outlook.es>
2023-12-31 02:04:41 -0600

    validacion de datos

--- src/main/java/com/xjgv/apiservlet/webapp/form/FormServlet.java
+++ src/main/java/com/xjgv/apiservlet/webapp/form/FormServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/form")
 public class FormServlet extends HttpServlet {
@@ -15,19 +17,43 @@ public class FormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String nombre = req.getParameter("name");
+        String precio = req.getParameter("price");
+        String fabricante = req.getParameter("manufacturer");
         resp.setContentType("text/html");
-        try (PrintWriter out = resp.getWriter()) {
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("     <head>");
-            out.println("         <meta charset=\" UTF-8\">");
-            out.println("         <title></title>");
-            out.println("     </head>");
-            out.println("     <body>");
-            out.println("         <h1></h1>");
-            out.println("     </body>");
-            out.println("</html>");
+        Map<String, String> errores = new HashMap<>();
+        if (nombre == null || nombre.isBlank()){
+            errores.put("name", "El nombre no puede estar vacío");
         }
+        if (precio == null || precio.isBlank()){
+            errores.put("price", "El precio no puede estar vacío");
+        }
+        if (fabricante == null || fabricante.isBlank()){
+            errores.put("manufacturer", "Debe especificar el fabricante");
+        }
+
+        if (errores.isEmpty()){
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("     <head>");
+                out.println("         <meta charset=\" UTF-8\">");
+                out.println("         <title></title>");
+                out.println("     </head>");
+                out.println("     <body>");
+                out.println("         <h1>Datos</h1>");
+                out.println("         <h1>Nombre: " + nombre + "</h1>");
+                out.println("         <h1>Precio: $" + precio + "</h1>");
+                out.println("         <h1>Fabricante: " + fabricante + "</h1>");
+                out.println("     </body>");
+                out.println("</html>");
+            }
+        } else {
+
+            req.setAttribute("errores", errores);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+        }
+
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <%@page contentType="text/html; ISO-8859-1" pageEncoding="utf-8" %>
+<%@page import="java.util.Map" %>
 
+<%
+    Map<String, String> errores = (Map<String, String>) request.getAttribute("errores");
+%>
 <!doctype html>
 <html lang="en" data-bs-theme="dark">
 <head>
@@ -16,25 +20,41 @@
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png" alt="Logo" width="30"
-                 height="24"
-                 class="d-inline-block align-text-top">
-            Bootstrap
+                 height="24" class="d-inline-block align-text-top"> Bootstrap
         </a>
     </div>
 </nav>
 
+<%
+    if (errores != null && errores.size() > 0) {
+%>
+<ul class="alert alert-danger my-5">
+    <% for (String error : errores.values()) {%>
+    <li><%=error%>
+    </li>
+    <% } %>
+</ul>
+<%
+    }
+%>
+
 <div class="container d-flex flex-column justify-content-center align-items-center">
     <h1>Formulario de envío</h1>
-    <form action="/webapp-form-tarea2/form" method="post">
-        <div class="mb-3 col-md-6">
+    <form action="/webapp-form-tarea2/form" method="post" class="col-8 needs-validation" novalidate>
+        <div class="mb-3">
             <label for="name" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Tu nombre">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Tu nombre" required>
+            <%
+                if (errores!= null && errores.containsKey("name")){
+                    out.println("<div class='invalid-feedback'>Debes ingresar tu nombre</div>");
+                }
+            %>
         </div>
-        <div class="mb-3 col-md-6">
+        <div class="mb-3">
             <label for="price" class="form-label">Precio</label>
             <input type="text" class="form-control" id="price" name="price" placeholder="$1,000.00">
         </div>
-        <div class="mb-3 col-md-6">
+        <div class="mb-3">
             <label for="name" class="form-label">Fabricante</label>
             <input type="text" class="form-control" id="manufacturer" name="manufacturer"
                    placeholder="Nombre de la compañia">
